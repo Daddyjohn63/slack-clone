17f034ce407f90b3073e2b4277cd602c48c7e351
Daddyjohn63 <john@webandprosper.co.uk>
2024-11-01 17:12:56 +0000

    added sidebar and buttons

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,18 +1,7 @@
 'use client';
 
-import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
-import { useWorkspaceId } from '@/hooks/use-workspace-id';
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-
-  return (
-    <div>
-      <span>Data: {JSON.stringify(data)}</span>
-      {`Workspace ${data?.name}`}
-    </div>
-  );
+  return <div>Workspace id page</div>;
 };
 
 export default WorkspaceIdPage;
