a08d2facbeb6f68059dff3e872873043fec4e554
Daddyjohn63 <john@webandprosper.co.uk>
2024-12-19 11:06:41 +0000

    can create new channels now

--- src/app/workspace/[workspaceId]/components/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/components/workspace-section.tsx
@@ -9,7 +9,7 @@ interface WorkspaceSectionProps {
   children: React.ReactNode;
   label: string;
   hint: string;
-  onNew: () => void;
+  onNew?: () => void;
 }
 
 export const WorkspaceSection: React.FC<WorkspaceSectionProps> = ({
