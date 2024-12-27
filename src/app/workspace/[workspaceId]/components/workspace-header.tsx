041ffd28367f4095d4504b0456b0322f47085fc9
Daddyjohn63 <john@webandprosper.co.uk>
2024-12-27 17:36:15 +0000

    added functionlaity to invite users to a channel

--- src/app/workspace/[workspaceId]/components/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/components/workspace-header.tsx
@@ -12,6 +12,7 @@ import { Hint } from '@/components/hint';
 import { Doc } from '../../../../../convex/_generated/dataModel';
 import { PreferencesModal } from './preferences-modal';
 import { useState } from 'react';
+import { InviteModal } from './invite-modal';
 
 interface WorkspaceHeaderProps {
   workspace: Doc<'workspaces'>;
@@ -23,8 +24,15 @@ export const WorkspaceHeader = ({
   isAdmin
 }: WorkspaceHeaderProps) => {
   const [preferencesOpen, setPreferencesOpen] = useState(false);
+  const [inviteOpen, setInviteOpen] = useState(false);
   return (
     <>
+      <InviteModal
+        open={inviteOpen}
+        setOpen={setInviteOpen}
+        name={workspace.name}
+        joinCode={workspace.joinCode}
+      />
       <PreferencesModal
         open={preferencesOpen}
         setOpen={setPreferencesOpen}
@@ -59,7 +67,7 @@ export const WorkspaceHeader = ({
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   className="cursor-pointer py-2"
-                  onClick={() => {}}
+                  onClick={() => setInviteOpen(true)}
                 >
                   Invite people to {workspace.name}
                 </DropdownMenuItem>
